4f04c59af3f30b6d0a1801740ecd4da1bf21cdc9
Fuji Halim Rabbani <racun1601@gmail.com>
2023-09-20 06:53:41 +0700

    fix: arrow navigation index

--- src/components/Search/micro/KeyShortcut.tsx
+++ src/components/Search/micro/KeyShortcut.tsx
@@ -22,29 +22,39 @@ const useKeyboardShortcuts = (openModal: () => void, closeModal: () => void) => 
   }, [openModal, closeModal]);
 };
 
-
-export const KeyboardNavigation: React.FC<KeyboardNavigationProps> = ({ selectedIndex, setSelectedIndex, itemRefs,}) => {
-
-const handleKeyDown = (event: KeyboardEvent) => {
-  if (event.key === 'ArrowUp' && selectedIndex !== null) {
-    if (selectedIndex === 0) {
-      setSelectedIndex(null);
-    } else {
-      setSelectedIndex(selectedIndex - 1);
+// handle arrow navigation and enter
+export const KeyboardNavigation: React.FC<KeyboardNavigationProps> = ({
+  selectedIndex,
+  setSelectedIndex,
+  itemRefs,
+}) => {
+  const handleKeyDown = (event: KeyboardEvent) => {
+    if (event.key === 'ArrowUp' && selectedIndex !== null) {
+      if (selectedIndex === 0) {
+        setSelectedIndex(null);
+      } else {
+        setSelectedIndex(selectedIndex - 1);
+      }
+    } else if (event.key === 'ArrowDown') {
+      if (selectedIndex === null || !itemRefs.current) {
+        setSelectedIndex(0);
+      } else if (selectedIndex === itemRefs.current.length - 1) {
+        setSelectedIndex(0);
+      } else {
+        setSelectedIndex(selectedIndex + 1);
+      }
+    } else if (event.key === 'Enter' && selectedIndex !== null) {
+      const selectedElement = itemRefs.current?.[selectedIndex];
+      if (selectedElement) {
+        selectedElement.scrollIntoView({
+          behavior: 'smooth',
+          block: 'nearest',
+          inline: 'nearest',
+        });
+        selectedElement.click();
+      }
     }
-  } else if (event.key === 'ArrowDown') {
-    if (selectedIndex === null || selectedIndex === (itemRefs.current?.length ?? 0) - 1) {
-      setSelectedIndex(0);
-    } else {
-      setSelectedIndex(selectedIndex + 1);
-    }
-  } else if (event.key === 'Enter' && selectedIndex !== null) {
-    const selectedElement = itemRefs.current?.[selectedIndex];
-    if (selectedElement) {
-      selectedElement.click();
-    }
-  }
-};
+  };
 
   useEffect(() => {
     const handleKeyPress = (event: KeyboardEvent) => {
@@ -56,7 +66,7 @@ const handleKeyDown = (event: KeyboardEvent) => {
     return () => {
       window.removeEventListener('keydown', handleKeyPress);
     };
-  }, [selectedIndex]);
+  }, [selectedIndex, itemRefs]);
 
   return null;
 };
