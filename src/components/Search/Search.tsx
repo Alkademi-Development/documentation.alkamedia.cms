1fa7ee74e6b7956752ef97a34948394eed759db8
fdvky1 <fdvkey@gmail.com>
2023-09-21 10:33:07 +0700

    fix: warn from react

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -15,7 +15,7 @@ export const Search = () => {
       >
         <div className="container bg-gray-100 dark:bg-gray-900 w-full mb-4 border-solid border-2 border-sky-500 flex justify-between">
           <svg width="25" height="25" className="text-indigo-700 mx-5 my-auto" viewBox="0 0 20 20">
-            <path d="M14.386 14.386l4.0877 4.0877-4.0877-4.0877c-2.9418 2.9419-7.7115 2.9419-10.6533 0-2.9419-2.9418-2.9419-7.7115 0-10.6533 2.9418-2.9419 7.7115-2.9419 10.6533 0 2.9419 2.9418 2.9419 7.7115 0 10.6533z" stroke="currentColor" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path>
+            <path d="M14.386 14.386l4.0877 4.0877-4.0877-4.0877c-2.9418 2.9419-7.7115 2.9419-10.6533 0-2.9419-2.9418-2.9419-7.7115 0-10.6533 2.9418-2.9419 7.7115-2.9419 10.6533 0 2.9419 2.9418 2.9419 7.7115 0 10.6533z" stroke="currentColor" fill="none" fillRule="evenodd" strokeLinecap="round" strokeLinejoin="round"></path>
           </svg>
           <SearchBox className="flex-grow" placeholder='Search docs' autoFocus />
         </div>
